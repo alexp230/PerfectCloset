95177340ac42bc70645cb79deafbfb60ca605db6
alexp230 <apruitt230@gmail.com>
2023-08-24 16:46:35 -0500

    Removed dead code Change formation of code to increase readability and stability

--- Calculator.py
+++ Calculator.py
@@ -13,6 +13,13 @@ from colorama import Fore, Back, Style
 #https://en.wikipedia.org/wiki/Magenta#/media/File:RBG_color_wheel.svg
 #https://www.canva.com/colors/color-wheel/
 
+
+
+PANTS_MATCH = {
+    "Beige": ("Aqua", "Blue", "Cyan", "Green", "Light_Blue", "Lime_Green", "Magenta", "Orange", "Pink", "Purple", "Red", "Yellow"),
+    "Dark_Blue": ("Green", "Light_Blue", "Lime_Green", "Yellow", "Orange"),
+}
+
 color_range_top = {
     "Red": (51,255, 0,204, 0,204),
     "Orange": (51,255, 25,229, 0,204),
@@ -52,156 +59,155 @@ ColorWheel = {
 
 
 
-# def createSquare(length: int, color: tuple, x: int, y: int, amount: int):
-#     """
-#     LENGTH: the size of the sides of the shape
-#     COLOR: a tuple containing the (R,G,B) values
-#     X: the x-coordinate of the start of the shapes being drawn
-#     Y: the y-coordinate of the start of the shapes being drawn
-#     AMOUNT: the amount of shapes being drawn
+def main(file_root: str, file_end: str):
 
-#     """
+    def pants_to_Shirt():
+
+        # Dictionary for the file path (pants)
+        color_options = {
+        "1": "Dark_Blue",
+        "2": "Beige",
+        }
+
+        # Gets user input to access correct name for file path (pants)
+        a = input("Choose your color of pants:\n"
+                "1. Dark Blue\n"
+                "2. Beige\n"
+        )
+
+        color_of_pants = color_options.get(a, "Invalid")
+
+        while (color_of_pants == "Invalid"):
+            a = input("\nInvalid choice, choose again\n")
+            color_of_pants = color_options.get(a, "Invalid")
+
+        # Builds file path to get the picture for the desired pair of pants
+        file_name = file_root + color_of_pants + file_end
+
+        # for test cases
+        # file_name = "/Users/aprui/Side_Projects/PerfectCloset/Test_Images/Red-Orange.png"
+
+
+        picture, image = Load_Picture(file_name)
+        x, y = image.size # Sets the value of dimensions to iterate over
+
+        RGB_value = getRGB(picture, x, y)
+
+        RGB_to_Color(RGB_value)
+        # This value determines the shirts to load in the next method
+        # List_Matches(color_of_pants)
     
-#     # Initialize turtle and modifies settings
-#     turtle.colormode(255)
-#     turtle.setup(900,700)
-#     t = turtle.Turtle()
-#     t.speed(15)
-#     t.pensize(3.5)
+#----------------------------------------------------------------------------------------------------
 
-    
-#     draw(t, x, y, amount, length, color, 200)
+    # Gets user input to access correct name for file path (pants)
+    option = input("Choose Settings:\n"
+            "1. Pants --> Shirt\n"
+            "2. Shirt --> Pants\n"
+    )
 
-    
-#     turtle.Screen().exitonclick()
-    
-#     return
-
-# def draw(t: turtle, x: int, y: int, amount: int, length: int, color: tuple, right_border: int):
-
-#     t.fillcolor(color)
-    
-#     for i in range(amount):
-        
-#         if (x > 350):
-#             x = -350
-#             y -= 200
-
-#         t.penup()
-#         t.goto(x,y)
-#         t.pendown()
-
-#         t.begin_fill()
-
-#         for i in range(4):
-#             t.forward(length)
-#             t.left(90)
-        
-#         t.end_fill()
-
-#         x += right_border
+    while (option not in ("1", "2")):
+        option = input("\nInvalid choice, choose again\n")
+        print(option)
+        print(type(option))
 
 
-def List_Matches(color_of_pants: str):
-
-    # This method will loop through the predifined list (depending on color of pants)
-    # and display photo of shirts using a forloop
-
-    file_root = "/Users/aprui/Side_Projects/Perfect_Closet/Shirts/"
-    file_end = ""
-
-    if (color_of_pants == "Dark_Blue"):
-        matches = ()
-    elif (color_of_pants == "Beige"):
-        matches = ()
+    startime = time.monotonic_ns()
 
 
+    if (option == "1"):
+        print("\n\n")
+        pants_to_Shirt()
 
-    for i in matches:
+    elif (option == "2"):
+        print("\n\n")
         pass
 
-    return
+    endtime = time.monotonic_ns()
 
-def getColor(picture: Image, length: int, width: int):
-    """
-    PICTURE: the picture of the shirt or the pair of pants
-    LENGTH: the length (x-coordinate) of the picture in pixels
-    WIDTH: the width (y-coordinate) of the picture in pixels
+    print(f'\n   {(endtime - startime)/10000000000:.4f}s   ')
+    return 
+# Main
 
-    returns: the color (in RGB) of the image
-    """
 
-    # Stores the amount red, green, and blue in each pixel
-    Red = 0
-    Green = 0
-    Blue = 0
-
-    # Keep track of iterations
-    denominator = 0
-
-    # calculates the total amount of red, green, and blue in each pixel (omit black and white colors)
-    for x in range(length):
-        for y in range(width):
-            r, g, b = picture[x,y]
-            #rgb_value = np.array([r,g,b])
-            
-
-            if (r <= 20 and b <= 20 and g <= 20): # Omits black
-                continue
-            if (r >= 230 and b >= 230 and g >= 230): # Omits white
-                continue
-
-            Red += r
-            Green += g
-            Blue += b
-
-            denominator += 1
-
-    #(44,52,88)
-    #(182,169,144)
-    # returns the average amount of red, green, and blue in the entire picture, getting us the color of the image
-    return ( (Red//denominator), (Green//denominator), (Blue//denominator) )
-
-def get_RGBvalue(file_path: str, color_of_pants: str):
+def Load_Picture(file_path: str):
     """
     FILE_PATH: the file path to the selected pair of pants or shirt
-    COLOR_OF_PANTS: the color of pants the user selected
-    """
-    PANTS_MATCH = {
-        "Beige": ("Aqua", "Blue", "Cyan", "Green", "Light_Blue", "Lime_Green", "Magenta", "Orange", "Pink", "Purple", "Red", "Yellow"),
-        "Dark_Blue": ("Green", "Light_Blue", "Lime_Green", "Yellow", "Orange"),
-    }
 
-    file_path = "/Users/aprui/Side_Projects/PerfectCloset/Test_Images/Orange.png"
+    Loads the image and converts the image into an "RGB" image
+
+    Returns: loaded image, image in RGB
+    """
 
     image = Image.open(file_path, "r") # Can be many different formats. Opens image
-
     image_rgb = image.convert("RGB") # Changes image to rgb format
-
     picture = image_rgb.load() # Allows us to get the size (coordinates) of the image
 
-    x, y = image_rgb.size # Sets the value of dimensions to iterate over
+    return (picture, image_rgb)
+# Load_Picture
 
-    RGB_value = getColor(picture, x, y)
-    
-    print(RGB_value) 
+def getRGB(picture: Image, length: int, width: int):
+        """
+        PICTURE: the picture of the shirt or the pair of pants
+        LENGTH: the length (x-coordinate) of the picture in pixels
+        WIDTH: the width (y-coordinate) of the picture in pixels
 
-    return(RGB_value)
+        Calculates the average amount of red, green, and blue in the entire picture, getting us 
+        the color of the image in RGB
+
+        Returns: the color (in RGB) of the image
+        """
+
+        # Stores the amount red, green, and blue in each pixel
+        Red = 0
+        Green = 0
+        Blue = 0
+
+        # Keep track of iterations
+        denominator = 0
+
+        # calculates the total amount of red, green, and blue in each pixel (omit black and white colors)
+        for x in range(length):
+            for y in range(width):
+                r, g, b = picture[x,y]
+                #rgb_value = np.array([r,g,b])
+                
+
+                if (r <= 20 and b <= 20 and g <= 20): # Omits black
+                    continue
+                if (r >= 230 and b >= 230 and g >= 230): # Omits white
+                    continue
+
+                Red += r
+                Green += g
+                Blue += b
+
+                denominator += 1
+
+        # returns the average amount of red, green, and blue in the entire picture, getting us the color of the image
+        return ( (Red//denominator), (Green//denominator), (Blue//denominator) )
+
 
 
 def RGB_to_Color(RGB_value: tuple):
     """
     RGB_VALUE: a tuple of the RGB values
 
+    Turns the RGB value into a string version of the color
+
     RETURN: the color (in string) of the color of shirt/pants
     """
     
     def percentage_difference(given: int, min_color: int, max_color: int, color: str):
         """
-        GIVEN: the value of R,G, or B, depending on the iteration
+        GIVEN: the value of R,G, or B (depending on the iteration)
         MIN_COLOR: the minimum color range of the 'GIVEN'
         MAX_COLOR: the maximum color range of the 'GIVEN'
         COLOR: the string and key of the 'color_range' dictionary
+
+        Finds the percentage of different amongst the values of red, green, or blue,
+        comparing the given RGB and the range of the value R, G, or B
+
+        Returns: percent difference of value
         """
 
         average = ((color_range.get(color)[max_color] + color_range.get(color)[min_color]) // 2)
@@ -211,6 +217,11 @@ def RGB_to_Color(RGB_value: tuple):
     def out_of_Range(color: str):
         """
         COLOR: the key for the dictionary to access min and max values of the color
+
+        Checks to see if a color is 'out of range' of another color (i.e the color red could never
+        mistaken for a green)
+
+        Returns: True (color is out of range) or False (color is in range)
         """
 
         # Used to access the min and max values in dictionary for each color
@@ -249,21 +260,6 @@ def RGB_to_Color(RGB_value: tuple):
     "White": (230,255, 230,255, 230,255)
 }
     
-    color_range = {
-    "Red": (51,255, 0,204, 0,204),
-    "Red-Orange": (51,255, 25,128, 0,204),
-    "Orange": (51,255, 25,229, 0,204),
-    "Yellow-Orange": (51,255, 25,255, 0,204),
-    "Yellow": (51,255, 51,255, 0,204),
-    "Yellow-Green": (51,204, 51,255, 0,204),
-    "Green": (0,204, 51,255, 0,204),
-    "Blue-Green": (0,204, 51,204, 51,204),
-    "Blue": (0,204, 0,204, 51,255),
-    "Blue-Purple": (25,204, 0,204, 51,255),
-    "Purple": (25,229, 0,204, 51,255),
-    "Red-Purple": (51,229, 0,204, 0,204),
-}
-    
     # Seperates the RGB values into own variable
     r, g, b = (RGB_value)
     
@@ -276,8 +272,8 @@ def RGB_to_Color(RGB_value: tuple):
     min_blue = 4
     max_blue = 5
 
-    True_combos = []
-    
+    combos = []
+    Inrange_combos = []
 
     # Goes through each color in the dictionary and find the potential values for each color
     for color in color_range.keys():
@@ -287,7 +283,7 @@ def RGB_to_Color(RGB_value: tuple):
         if (out_of_Range(color)):
             x = True
 
-        rank = [color,0,0,0,0,0]
+        rank = [color,0,0,0,0]
 
         rank[1] = percentage_difference(r,min_red, max_red, color)
         rank[2] = percentage_difference(g,min_green, max_green, color)
@@ -295,99 +291,26 @@ def RGB_to_Color(RGB_value: tuple):
         
         rank[4] = round(((rank[1] + rank[2] + rank[3]) / 3), 2)
 
-        rank[5] = x
+        combos.append(rank)
 
-        True_combos.append(rank)
-
-
-    combos = []
-    for i in True_combos:
-        if (i[5] == False):
+        if (x == False):
             print("In Range", end=" ")
-            print(i)
-            combos.append(i)
+            print(rank)
+            Inrange_combos.append(rank)
         else:
             print("Out of Range", end=" ")
-            print(i)
-
-            
+            print(rank)
+    
 
     # Returns the color of the shirt/pants
-    if (len(combos) == 1):
-        return combos[0][0]
+    if (len(Inrange_combos) == 1):
+        return Inrange_combos[0][0]
     
     # Finds color value which has the least percent difference 
-    sorted_items = sorted(combos, key=lambda x: x[4])
+    sorted_items = sorted(Inrange_combos, key=lambda x: x[4])
+    print(sorted_items[0][0])
     return sorted_items[0][0]
-
-
-
-def main(file_root: str, file_end: str):
-
-    #createSquare(100, (0,0,204), -350, 200, 4)
-
-    def pants_to_Shirt():
-
-        # Dictionary for the file path (pants)
-        color_options = {
-        "1": "Dark_Blue",
-        "2": "Beige",
-        }
-
-        # Gets user input to access correct name for file path (pants)
-        a = input("Choose your color of pants:\n"
-                "1. Dark Blue\n"
-                "2. Beige\n"
-        )
-
-        color_of_pants = color_options.get(a, "Invalid")
-
-        while (color_of_pants == "Invalid"):
-            
-            a = input("\nInvalid choice, choose again\n")
-
-            color_of_pants = color_options.get(a, "Invalid")
-
-        # Builds file path to get the picture for the desired pair of pants
-        file_name = file_root + color_of_pants + file_end
-
-        # Gets the RGB_value/Color of selected pair of pants
-        RGB_value = get_RGBvalue(file_name, color_of_pants)
-
-        print(RGB_to_Color(RGB_value))
-        # This value determines the shirts to load in the next method
-        #List_Matches(color_of_pants)
-
-
-    
-
-    # Gets user input to access correct name for file path (pants)
-    option = input("Choose Settings:\n"
-            "1. Pants --> Shirt\n"
-            "2. Shirt --> Pants\n"
-    )
-
-    while (option not in ("1", "2")):
-        
-        option = input("\nInvalid choice, choose again\n")
-        print(option)
-        print(type(option))
-
-
-    startime = time.monotonic_ns()
-
-    if (option == "1"):
-        print("\n\n")
-        pants_to_Shirt()
-
-    elif (option == "2"):
-        print("\n\n")
-        pass
-
-    endtime = time.monotonic_ns()
-
-    print(f'\n   {(endtime - startime)/10000000000:.4f}s   ')
-    return
+# getRGB
 
 
 """
